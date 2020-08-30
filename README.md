375d4aba22a4178da735be1c886c2ec13fdd5434
miloni9 <milonishah9196@gmail.com>
2020-08-31 00:33:01 +0530

    readme.md update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# git-event-pub-sub
+#Event PubSub with Kafka 
+
+### Steps for depoying the architecture
+
+<p>
+**Step 1**
+
+Build the Docker image using dockerfile from the jar file 
+
+`docker build -t localhost:5000/git-event-pub-sub:1.0 -f Dockerfile .
+` 
+</p>
+
+<p>
+**Step 2**
+
+Push the docker image on local docker registry
+
+` docker push  localhost:5000/git-event-pub-sub:1.0
+`
+</p>
+
+<p>
+**Step 3**
+
+Run the following command to run the application services 
+ 
+` docker-compose -f docker-compose.yml up -d`
+</p>
--- src/test/java/service/IngestEventDataTest.java
+++ src/test/java/service/IngestEventDataTest.java
@@ -17,6 +17,8 @@ public class IngestEventDataTest {
 
     IngestEventData ingestEventData;
 
+
+
     private Event getEventData() {
 
         String string = "{\"actor\":{\"id\":66008870,\"login\":\"tidorisan\",\"display_login\":\"tidorisan\",\"gravatar_id\":\"\",\"url\":\"https://api.github.com/users/tidorisan\",\"avatar_url\":\"https://avatars.githubusercontent.com/u/66008870?\"},\"public\":true,\"payload\":{\"issue\":null,\"action\":null,\"comment\":null,\"push_id\":5603629066,\"size\":2,\"distinct_size\":1,\"ref\":\"refs/heads/develop\",\"head\":\"85bb86e72466adbe782a6bb73d6fd37f498256a3\",\"before\":\"c4cb8bae3eb862b1db773636acce1a3e863a31cf\",\"commits\":[{\"sha\":\"f8a49f1b7fda117a215d7134637672eaad3ebd59\",\"author\":{\"email\":\"vagrant@localhost.localdomain\",\"name\":\"vagrant\"},\"message\":\"[create]_coupons/index/new/edit/show\",\"distinct\":false,\"url\":\"https://api.github.com/repos/tidorisan/TIdori_sushi_app/commits/f8a49f1b7fda117a215d7134637672eaad3ebd59\"},{\"sha\":\"85bb86e72466adbe782a6bb73d6fd37f498256a3\",\"author\":{\"email\":\"66008870+tidorisan@users.noreply.github.com\",\"name\":\"tidorisan\"},\"message\":\"Merge pull request #7 from tidorisan/feature_coupon_index/new/show/edit\\n\\n[create]_coupons/index/new/edit/show\",\"distinct\":true,\"url\":\"https://api.github.com/repos/tidorisan/TIdori_sushi_app/commits/85bb86e72466adbe782a6bb73d6fd37f498256a3\"}]},\"repo\":{\"id\":290931874,\"name\":\"tidorisan/TIdori_sushi_app\",\"url\":\"https://api.github.com/repos/tidorisan/TIdori_sushi_app\"},\"created_at\":\"2020-08-29T08:24:35Z\",\"id\":13346508506,\"type\":\"PushEvent\"}\n" +
